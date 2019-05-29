250edaf08412f96e03f9e3f51438f735bd1a0bfd
Shiheng Dong <dongshiheng9@gmail.com>
2019-05-28 21:40:32 -0400

    replace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { MaskComponent } from './mask/mask.component';
+import { WarningAlertComponent} from './warning-alert/warning-alert-component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    MaskComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
